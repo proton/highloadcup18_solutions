1f1a81e3f1e93fcc124839d0a5f4ad4a429e9b3f
Peter Savichev (proton) <psavichev@gmail.com>
2019-02-16 17:32:37 +0100

    updated list

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 
 | Место  | Ссылка | Язык | Штраф | Имя |
 | ------------- | ------------- | ------------- | ------------- | ------------- |
-|  | https://github.com/covrom/highloadcup2018 | | | Роман Цованян |
-|  | https://github.com/solyutor/hlcup2018 | | | Jury Soldatenkov |
-|  | https://github.com/Hartigan/highloadcup2018 | | | Анатолий Сафонов |
+| 24 | https://github.com/solyutor/hlcup2018 | C# | 287.86844 | Jury Soldatenkov |
+| 28 | https://github.com/covrom/highloadcup2018 | Go | 410.52697 | Роман Цованян |
+| 45 | https://github.com/Hartigan/highloadcup2018 | C# | 34632.96135 | Анатолий Сафонов |
 |  | https://github.com/proton/highloadcup18_node.js | Node.js |  | Peter Savichev |
 
 Or you can search by tag: https://github.com/search?q=topic%3Ahighloadcup&type=Repositories
--- updater.rb
+++ updater.rb
@@ -2,7 +2,7 @@ require 'mechanize'
 require 'pry'
 
 README_FILE = './README.md'
-NO_TIME = Float::INFINITY
+NO_PLACE = Float::INFINITY
 URL = 'https://highloadcup.ru/ru/rating/'
 
 repos = {}
@@ -14,12 +14,11 @@ file_table_lines = file_table_content.split("\n")
 file_table_header = file_table_lines[0..1]
 file_table_lines[2..-1].each do |line|
   _, place, url, lang, time, name, _ = line.split('|').map(&:strip)
-  place = place.to_i
+  place = !place.empty? ? place.to_i : nil
   name = url if name.empty?
-  lang = nil if lang.blank?
-  time = time.to_f
-  time = Float::INFINITY if time.zero?
-  repos[name] = { place: place, url: url, lang: lang, time: time.to_f, name: name }
+  lang = nil if lang.empty?
+  time = time.to_f unless time.empty?
+  repos[name] = { place: place, url: url, lang: lang, time: time, name: name }
 end
 
 page = agent.get(URL)
@@ -45,15 +44,12 @@ user_rows.each_with_index do |user_row, i|
 end
 
 fields = %i(place url lang time name)
-new_table_rating = repos.values.sort_by { |repo| [repo[:place], repo[:name]] }.map do |repo|
+new_table_rating = repos.values.sort_by { |repo| repo[:place] || NO_PLACE }.map do |repo|
   repo[:name] = nil if repo[:name].start_with? 'http'
-  repo[:time] = nil if repo[:time].infinite?
-  time = repo[:time].to_f
-  # repo[:time] = time.round(2) if time.to_i.to_s.size > 4
   ([nil] + fields.map { |f| repo[f] } + [nil]).join(' | ').strip
 end
 new_file_table_content = (file_table_header+new_table_rating).join("\n")
 puts new_file_table_content
 
 file_content.gsub!(file_table_content, new_file_table_content)
-# File.open(README_FILE, 'w') { |f| f.write file_content }
+File.open(README_FILE, 'w') { |f| f.write file_content }
